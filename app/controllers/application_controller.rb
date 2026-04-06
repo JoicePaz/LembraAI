912e9890ea279d6ade1be746fbf503070f9601aa
Joice Paz <joicepazsilva@gmail.com>
2026-04-06 00:05:06 -0300

    Add files via upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_owner_token
+  helper_method :current_owner_token
+
+  private
+
+  def ensure_owner_token
+    return if cookies.signed[:owner_token].present?
+
+    cookies.permanent.signed[:owner_token] = {
+      value: SecureRandom.hex(16),
+      httponly: true,
+      same_site: :lax
+    }
+  end
+
+  def current_owner_token
+    cookies.signed[:owner_token]
+  end
 end
