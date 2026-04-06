a1b5260f5e5ff3a3a2cc0a8da2787f9a75f27d52
Joice Paz <joicepazsilva@gmail.com>
2026-04-06 00:06:12 -0300

    Add files via upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @has_decks = Deck.table_exists? && Deck.exists?
+    @has_decks = Deck.table_exists? && Deck.where(owner_token: current_owner_token).exists?
   end
 end
